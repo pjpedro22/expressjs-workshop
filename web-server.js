ca46b4be1b9ee830685ea034acc0eebda20adb0b
Guillaume Légaré <legare.guillaume.1990@gmail.com>
2017-07-18 21:38:54 -0400

    'local'

--- web-server.js
+++ web-server.js
@@ -4,18 +4,18 @@ var express = require('express');
 var app = express();
 
 //exercise 4
-var RedditAPI = require("../reddit-nodejs-api/reddit.js");
+var RedditAPI = require("./reddit.js");
 var request = require('request-promise');
 var mysql = require('promise-mysql');
 
   var connection = mysql.createPool({
     host     : 'localhost',
-    user     : 'pjpedro22',
-    password : '',
+    user     : 'root',
+    password : 'root',
     database : 'reddit',
     connectionLimit: 10
   });
-  
+
     //Use myReddit to insert new data.
   var myReddit = new RedditAPI(connection);
 
@@ -31,27 +31,27 @@ app.get('/hello', function (req, res) {
 });
 
 app.get('/calculator/:operation', function (req, res) {
-  
-  
-  
+
+
+
   var num1 = parseInt(req.query.num1);
   var num2 = parseInt(req.query.num2);
-  
+
   console.log(req.params);
   console.log(req.query);
-  
+
   //equal to the correct params key
   var operation = req.params.operation;
   var solution;
-  
+
   if (!req.query.num1 ) {
     num1 = 0;
-  
+
   }
   else if(!req.query.num2){
     num2 = 0;
   }
-  
+
   else if(!req.query.num2 && !req.query.num1 ){
     num1 = 0;
     num2 = 0;
@@ -60,15 +60,15 @@ app.get('/calculator/:operation', function (req, res) {
         if (operation === 'add') {
           solution = num1 + num2;
         }
-        
+
         else if (operation === 'mult') {
           solution = num1 * num2;
         }
-        
+
         else {
           res.status(400).send(`Bad request`);
         }
-    
+
 
   var operationObject = {
       "operation": operation,
@@ -76,19 +76,19 @@ app.get('/calculator/:operation', function (req, res) {
       "secondOperand": num2,
       "solution": solution
     };
-    
+
     res.end(JSON.stringify(operationObject, null, 2));
 });
 
 app.get('/posts', function(req, res) {
 
-  var myHTMLString = `
-  <div id="posts">
-    <h1>List of posts</h1>
-      <ul class="posts-list">`;
-  
-  myReddit.getAllPosts()
+myReddit.getAllPosts()
   .then(dbPost => {
+    var myHTMLString = `
+    <div id="posts">
+      <h1>List of posts</h1>
+        <ul class="posts-list">`;
+
     dbPost.forEach(post => {
       //looking for post value.
       console.log(post, 'This is my post value');
@@ -97,17 +97,19 @@ app.get('/posts', function(req, res) {
           <h2 class="post-item__title">
             <a href=`+ post.url + `>`+ post.title + `</a>
           </h2>
-          <p>Created by ` + post.users.username + `</p>
+          <p>Created by ` + post.username + `</p>
         </li>`;
     });
-  })
-  .then(result => {
+
+
     myHTMLString += `
-    </ul>
-  </div>`;
-  res.end(myHTMLString);
-  });
-});
+      </ul>
+    </div>`;
+  res.send(myHTMLString);
+
+});//end of then
+
+});//end of app.get /posts
 
 
 
@@ -117,6 +119,6 @@ app.get('/posts', function(req, res) {
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
-  console.log('Example app listening at http://%s', process.env.C9_HOSTNAME);
+var server = app.listen(4242, function () {
+  console.log('Example app listening at http://localhost:4242');
 });
